1284e797ca0b42bb3e8071f59e5380d96c8b02e0
이상훈 <00aaaa00@naver.com>
2018-01-22 10:38:15 +0900

    bb

--- GitTest/src/main/java/com/git/test/HomeController.java
+++ GitTest/src/main/java/com/git/test/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		System.out.println("³ª´Â µÅÁö¸¸ ... ÈÞ ¾¾¹ß!");
 		return "home";
 	}
 	
